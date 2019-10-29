4ab64471019aa09a8bf950368341856318009878
Thomas Hitz <thitz@uncc.edu>
2019-10-29 16:47:01 +0000

    added currency formating, added sum functions to wishlist and expenses, added sum display to welcome page

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,7 @@
 class Expense < ApplicationRecord
+     has_many :expenses
      validates :description, presence: true, length: { minimum: 1 }
     validates :price, presence: true, length: { minimum: 1 }
+   
+
 end
