9cfe3a754d5baccfbcbc21fe9962e85decb6cd94
Thomas Hitz <thitz@uncc.edu>
2019-10-29 18:46:59 +0000

    added ability for decimals to be added to prices. added requirment the price is greater than 0

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,7 +1,7 @@
 class Expense < ApplicationRecord
      has_many :expenses
      validates :description, presence: true, length: { minimum: 1 }
-    validates :price, presence: true, length: { minimum: 1 }
+    validates :price, presence: true, length: { minimum: 1 }, numericality: {greater_than: 0}
    
 
 end
--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -1,4 +1,4 @@
 class Wishlist < ApplicationRecord
      validates :description, presence: true, length: { minimum: 1 }
-    validates :price, presence: true, length: { minimum: 1 }
+     validates :price, presence: true, length: { minimum: 1 }, numericality: {greater_than: 0}
 end
