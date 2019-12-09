a7ba5c73225ff3b0088467e94d40aa364c42aa08
EC2 Default User <ec2-user@ip-172-31-7-91.us-east-2.compute.internal>
2019-12-09 16:15:56 +0000

    Updated RSpec Tests

--- spec/integration/user_adds_an_expense_spec.rb
+++ spec/integration/user_adds_an_expense_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper.rb'
 feature"User views description of expense" do
     scenario "User successfully views description of expense" do
         visit expenses_path
-        expect(page).to have_content("Listing Expenses")
+        expect(page).to have_content("FUNDs")
         click_link "New Expense"
-        expect(page).to have_content("New Expense")
+        expect(page).to have_content("Please Enter the Information to Add an Expense")
         expect(page).to have_field("Description")
     end
 end
@@ -13,9 +13,9 @@ end
 feature"User views price of expense" do
     scenario "User successfully views price of expense" do
         visit expenses_path
-        expect(page).to have_content("Listing Expenses")
+        expect(page).to have_content("FUNDs")
         click_link "New Expense"
-        expect(page).to have_content("New Expense")
+        expect(page).to have_content("Please Enter the Information to Add an Expense")
         expect(page).to have_field("Price")
     end
 end
@@ -23,7 +23,7 @@ end
 feature"User views description of items on wishlist" do
     scenario "User successfully views description of items on wishlist" do
         visit wishlists_path
-        expect(page).to have_content("Listing WishList")
+        expect(page).to have_content("FUNDs")
         click_link "New Wishlist Item"
         expect(page).to have_content("Add WishList Item")
         expect(page).to have_field("Description")
@@ -33,7 +33,7 @@ end
 feature"User views price of items on wishlist" do
     scenario "User successfully views price of items on wishlist" do
         visit wishlists_path
-        expect(page).to have_content("Listing WishList")
+        expect(page).to have_content("FUNDs")
         click_link "New Wishlist Item"
         expect(page).to have_content("Add WishList Item")
         expect(page).to have_field("Price")
